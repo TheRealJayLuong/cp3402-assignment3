c37eef3ccb19b8d75f6d3ced579d49fb91bd6950
Jay <quocphong.luong@my.jcu.edu.au>
2021-01-20 01:18:17 +1000

    setting up thumbnail post columns

--- movie-review-columns.php
+++ movie-review-columns.php
@@ -23,4 +23,34 @@ function setting_up_columns_for_post($columns){
 add_action('manage_post_posts_columns','setting_up_columns_for_post');
 
 
+// setting up thumbnail columns for post type
+function setting_up_thumbnail_columns($column,$post_id){
+
+    //featured image column
+    if($column == 'post_featured_image'){
+        //if this post has a featured image
+        if(has_post_thumbnail($post_id)){
+            // Original: $post_featured_image = get_the_post_thumbnail($post_id,'thumbnail');
+            echo the_post_thumbnail( array(100,100) );
+        }else{
+            echo 'This post has no featured image'; 
+        }
+    }
+
+    
+    //post content column
+    if($column == 'post_content'){
+
+        //get the page based on its post_id
+        $post = get_post($post_id);
+        if($post){
+            //get the main content area
+            $post_content = apply_filters('the_content', $post->post_content); 
+            // Original: echo $post_content;  
+            echo wp_trim_words(get_the_content(), 10);
+        }
+    }
+}
+add_action('add_post_posts_custom_column','setting_up_thumbnail_columns',10,2);
+
 
