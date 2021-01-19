e924f9fd92dae062fc0da9420ba8bfbbfea82722
Jay <quocphong.luong@my.jcu.edu.au>
2021-01-20 00:35:00 +1000

    Plugin information

---
+++ movie-post-type.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Plugin Name: Movie Review Plugin J@
+  Plugin URI: 
+  Description: Create a Movie Custom Post type and a backend adjustment of the post and custom post type list screens.
+  Version: 1.0.0
+  Author: Jay Luong
+  License: GPLv2 or later
+  
+  */
+  
+  require_once("movie-post-type-function.php");
+  require_once("movie-review-columns.php");
+  
+  
+  ?>
